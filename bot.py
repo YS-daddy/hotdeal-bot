10d0d74ee00c3483246600b43f15d3ad9304c010
YS-daddy <1020won@gmail.com>
2026-03-10 22:12:07 +0900

    Simplify send function in bot.py
    Refactor send function to use a fixed success message and remove unnecessary parameters.

--- bot.py
+++ bot.py
@@ -1,22 +1,13 @@
 import requests
 
-TOKEN = "8473111628:AAHtQaEyPl89VDKMRqMvtPV1pxTWjHFpcEg"
-CHAT_ID = "1890536088"
+TOKEN="8473111628:AAHtQaEyPl89VDKMRqMvtPV1pxTWjHFpcEg"
+CHAT_ID="1890536088"
 
-def send(msg):
+url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
 
-    url=f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+data={
+    "chat_id":CHAT_ID,
+    "text":"🔥 텔레그램 연결 테스트 성공"
+}
 
-    data={
-        "chat_id":CHAT_ID,
-        "text":msg
-    }
-
-    r=requests.post(url,data=data)
-
-    print(r.text)
-
-
-if __name__=="__main__":
-
-    send("✅ 핫딜봇 테스트 성공")
+requests.post(url,data=data)
