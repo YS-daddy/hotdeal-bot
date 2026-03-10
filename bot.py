cef399be55ae14ac3f12f72ead98412e6ac20a75
YS-daddy <1020won@gmail.com>
2026-03-10 22:27:09 +0900

    Refactor bot.py for cleaner variable assignment

--- bot.py
+++ bot.py
@@ -1,15 +1,18 @@
 import requests
 
-TOKEN = "8473111628:AAHtQaEyPl89VDKMRqMvtPV1pxTWjHFpcEg"
-CHAT_ID = "1890536088"
+TOKEN="8473111628:AAHtQaEyPl89VDKMRqMvtPV1pxTWjHFpcEg"
+CHAT_ID="1890536088"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+print("bot start")
 
-data = {
-    "chat_id": CHAT_ID,
-    "text": "🔥 GitHub 서버 테스트 메시지"
+url="https://api.telegram.org/bot"+TOKEN+"/sendMessage"
+
+data={
+"chat_id":CHAT_ID,
+"text":"🔥 GitHub 테스트 메시지"
 }
 
-r = requests.post(url, data=data)
+r=requests.post(url,data=data)
 
+print(r.status_code)
 print(r.text)
